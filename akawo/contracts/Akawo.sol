27c9dca525e508f892df5b19f155561cc73bd48d
smartychuks <Smirhty@gmail.com>
2022-11-28 14:23:52 +0100

    earninings

--- akawo/contracts/Akawo.sol
+++ akawo/contracts/Akawo.sol
@@ -1,61 +1,104 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity-2.3.0/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity-2.3.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity-2.3.0/contracts/token/ERC20/SafeERC20.sol";
+// import "openzeppelin-solidity-2.3.0/contracts/utils/ReentrancyGuard.sol";
 
 contract Akawo{
-    ERC20 _token;
-    address _owner;
-    uint locktime = 0;
-    mapping(address => uint) public balancesFlexible;
-    mapping(address => uint) public balancesFixed;
-    // @map: account selected 0 for flexible and 1 for fixed
-    mapping(address => uint) public account;
-    mapping(address => uint) public time;
-    mapping(address => uint) public earnTime;
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable tokenAddress;
+    // Timestamp when reward finishes
+    uint256 public periodFinish = 0;
+    //Reward to be paid per second
+    uint256 public rewardRate = 0;
+    // Duration of rewards to be paid out
+    uint256 public rewardsDuration = 1 days;
     
+    uint256 public lastUpdateTime;
+    // reward to be paid per token
+    uint256 public rewardPerTokenStored;
+    address public owner;
+    uint256 locktime = 0;
+
+    // Storage to keep track of balance of each address in flexible account
+    mapping(address => uint256) public balancesFlexible;
+    // Keeps track of user fixed account balanace
+    mapping(address => uint256) public balancesFixed;
+    // @map: account selected 0 for flexible and 1 for fixed
+    mapping(address => uint256) public account;
+    mapping(address => uint256) public time;
+    mapping(address => uint256) public earnTime;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    // Rewards for each address
+    mapping(address => uint256) public rewards;
+    // Mapping for rates for different accounts
+    mapping(uint256 => uint256) public rewardRates;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;    
 
 
     // modifier for only owner
     modifier onlyOwner{
-        require(msg.sender == _owner, "Operation not allowed");
+        require(msg.sender == owner, "Operation not allowed");
         _;
     }
 
     // modifier to ensure only holders of AKW can access the features
     modifier onlyHolders{
-        address token = 0xf773e8fAdc25F35d43355a1204218a5E4B8529de;
-        require(ERC20(token).balanceOf(msg.sender) >= 1, "You need at least 1 AKW token in wallet");
+        //address token = 0xf773e8fAdc25F35d43355a1204218a5E4B8529de;
+        require(IERC20(tokenAddress).balanceOf(msg.sender) >= 1, "You need at least 1 AKW token in wallet");
         _;
     }
 
-    constructor(){
-        _owner = msg.sender;
+    modifier updateReward(address _account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (_account != address(0)) {
+            rewards[account] = earned(_account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    constructor(address _tokenAddress){
+        owner = msg.sender;
+        tokenAddress = IERC20(_tokenAddress);
     }
 
     //function to deposit
-    function deposit(uint256 _amount, ERC20 token) public onlyHolders{
-        _token = ERC20(token);
-        _token.transferFrom(msg.sender, address(this), _amount);        
+    function deposit(uint256 _amount) public updateReward onlyHolders{
+        require(_amount > 0, "Cannot Deposit 0");
+       
+        tokenAddress.transferFrom(msg.sender, address(this), _amount);        
         
         // Conditional to check which account to deposit to
         if(account[msg.sender] == 0){
-            balancesFlexible[msg.sender] += _amount;
+            rewardRates[0] = 1.div(9);
+            balancesFlexible[msg.sender] = balancesFlexible[msg.sender].add(_amount);
         }else if(account[msg.sender] == 1){
-            balancesFixed[msg.sender] += _amount;
-            
+            rewardRates[1] = 1.div(5);
+            balancesFixed[msg.sender] = balancesFixed[msg.sender].add(_amount);
             //lock and add one minute locktime for each deposit
             if (locktime < block.timestamp){
-                locktime = block.timestamp + 1 minutes;
-                time[msg.sender] += locktime;
+                locktime = block.timestamp.add(1 minutes);
+                time[msg.sender] = time[msg.sender].add(locktime);
             }
         }
+        _balances[msg.sender] = _balances[msg.sender].add(_amount);
+        _totalSupply = _totalSupply.add(_amount);
+        rewardRate = rewardRates[0].add(rewardRates[1]);
     }
 
     // function to set earn time to 24 hours
     function setEarnTime() public onlyHolders{
         require(block.timestamp > earnTime[msg.sender], 'Your earning session has not expired');
         // check if user already in earning circle     
-        earnTime[msg.sender] = block.timestamp + 1 days;
+        earnTime[msg.sender] = block.timestamp.add(1 days);
     }
 
     // function to get Earn time
@@ -67,20 +110,39 @@ contract Akawo{
     }
 
     // function to withdraw
-    function withdraw(uint256 _amount, ERC20 token) public onlyHolders{
-        _token = ERC20(token);
+    function withdraw(uint256 _amount) public updateReward(msg.sender) onlyHolders{
+        require(_amount > 0, "Can not withdraw 0 value");
+        
         if(account[msg.sender] == 0){// For flexible account
             require(balancesFlexible[msg.sender] > 0, "Flexible Balance is empty");
             require(balancesFlexible[msg.sender] >= _amount, "Insufficient flexible account Balance");   
-            _token.transfer(msg.sender, _amount);
-            balancesFlexible[msg.sender] -= _amount;
+            tokenAddress.transfer(msg.sender, _amount);
+            balancesFlexible[msg.sender] = balancesFlexible[msg.sender].sub(_amount);
+            if(!(balancesFlexible[msg.sender] > 0)) {
+                rewardRates[0] = 0;
+            }
+            
         }else if(account[msg.sender] == 1){// for fixed account
             require(balancesFixed[msg.sender] > 0, "Fixed saving account Balance is empty");
             require(balancesFixed[msg.sender] >= _amount, "Insufficient fixed account Balance");   
             require(block.timestamp > time[msg.sender], "Time for withdrawal has not reached");
-            _token.transfer(msg.sender, _amount);
-            balancesFixed[msg.sender] -= _amount;
+            tokenAddress.transfer(msg.sender, _amount);
+            balancesFixed[msg.sender] = balancesFixed[msg.sender].sub(_amount);
+            if(!(balancesFixed[msg.sender] > 0)) {
+                rewardRates[1] = 0;
+            }
         }
+        _balances[msg.sender] = _balances[msg.sender].sub(_amount);
+        _totalSupply = _totalSupply.sub(_amount);   
+    }
+
+    // Function to withdraw Earnings
+    function withdrawEarn() public updateReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            tokenAddress.safeTransfer(msg.sender, reward);
+        }    
     }
 
     // function to set account type
@@ -119,4 +181,53 @@ contract Akawo{
     function isDOwner() public view returns (address) {
         return _owner;
     }
+
+    // Funtion to get user's total balance of amount locked
+    function totalSupply external view returns (uint256) {
+        return _totalSupply;
+    }
+    
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return block.timestamp < periodFinish ? block.timestamp : periodFinish;
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {// If users have withdrawn all tokens saved
+            return rewardPerTokenStored;
+        }
+        return rewardPerTokenStored.add(lastTimeRewardApplicable()
+        .sub(lastUpdateTime).mul(rewardRate).mul(1e18)div(_totalSupply));
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+        .div(1e18).add(rewards[account]);
+    }
+
+    function getRewardForDuration() external view returns (uint256) {
+        return rewardRate.mul(rewardsDuration);
+    }
+
+    // Function to notify about reward amount
+    function notifyRewardAmount(uint256 reward) external onlyOwner updateReward(address(0)) {
+        if (block.timestamp >= periodFinish) {
+            rewardRate = reward.div(rewardsDuration);
+        }else{
+            uint256 remaining = periodFinish.sub(block.timestamp);
+            uint256 leftover = remaining.mul(rewardRate);
+            rewardRate = reward.add(leftover).div(rewardsDuration);
+        }
+
+        uint balance = tokenAddress.balanceOf(address(this));
+        require(rewardRate <= balance.div(rewardsDuration), "The reward provided is too high");
+
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp.add(rewardsDuration);
+    }
+
+
 }
